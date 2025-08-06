18e07dfdf4d55b2808fc67851f37adee4442db0c
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-08-06 16:47:22 +0300

    Update configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierConfig from 'eslint-config-prettier';
+import pluginEsLintImport from 'eslint-plugin-import';
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +13,27 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      'simple-import-sort': pluginSimpleImportSort,
+      import: pluginEsLintImport,
+    },
+    rules: {
+      // simple-import-sort rules
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
+      // eslint-plugin-import rules
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'import/max-dependencies': ['warn', { max: 10, ignoreTypeImports: true }],
+      'max-len': ['warn', { code: 100 }],
+      semi: [2, 'always'],
+    },
+  },
+  prettierConfig,
 ];
 
 export default eslintConfig;
